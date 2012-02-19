c5a72c40bc29fd27453ce43bed66b1a2a1a9e127
Dhaval Patel <dpatel@linkedin.com>
2012-02-18 23:10:39 -0800

    modifying meetup.py

--- common/meetup.py
+++ common/meetup.py
@@ -1,7 +1,7 @@
 import requests, json
 
 def get_upcoming_events():
-    resp = requests.get('https://api.meetup.com/2/open_events?key=512d2747301844127348784e541d78&sign=true&state=CA&status=upcoming&city=San Francisco&country=USA&topic=technology&zip=94110&radius=50&order=time&page=30&desc=true')
+    resp = requests.get('https://api.meetup.com/2/open_events?key=512d2747301844127348784e541d78&sign=true&state=CA&status=upcoming&city=San Francisco&country=USA&time=,1w&topic=technology&zip=94110&radius=50&order=time&page=30&desc=true')
     parsed = json.loads(resp.content)
     return parsed
 
