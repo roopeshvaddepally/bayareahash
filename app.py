fa076e9e204fd000868511a4f798a5c01b378320
Roopesh Vaddepally <roopeshvaddepally@gmail.com>
2012-02-18 21:38:56 -0800

    moved code around

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
+from common.db import subscribe
+from common.mail import send_email
 
 app = Flask(__name__)
 
-def store_email(email): pass
-def send_email(email, token): pass
 
 @app.route("/")
 def index():
 	return render_template("index.html")
 
+
 @app.route("/subscribe")
 def subscribe():
 	email = request.values.get("email")
-	token = store_email(email)
+	token = subscribe(email)
 	send_email(email, token)
 	return ''
 
-@app.route("/subscribed")
-def subscribed():
-	return "thanks for subscribing"
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- common/db.py
+++ common/db.py
@@ -2,7 +2,8 @@ import redis
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-def subscribe(email):pass
+def subscribe(email):
+	pass
 
 def confirm(email):pass
 
