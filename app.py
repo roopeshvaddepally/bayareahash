04ad41bfc9978601c4cb58a2774e8ab6e52fe687
Roopesh Vaddepally <roopeshvaddepally@gmail.com>
2012-02-18 21:15:49 -0800

    subscribe function chunked out

--- app.py
+++ app.py
@@ -2,9 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def store_email(email): pass
+def send_email(email, token): pass
+
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+@app.route("/subscribe")
+def subscribe():
+	email = request.values.get("email")
+	token = store_email(email)
+	send_email(email, token)
+	return redirect(url_for("/subscribed"))
+
+@app.route("/subscribed")
+def subscribed():
+	return "thanks for subscribing"
 
 if __name__ == '__main__':
 	app.run(debug=True)
