2aa61c28d0ade1328830df0aa30a73fac90367ae
Roopesh Vaddepally <roopeshvaddepally@gmail.com>
2012-02-18 22:35:31 -0800

    mongodb queries and stuff

--- common/db.py
+++ common/db.py
@@ -1,8 +1,22 @@
+from settings import data_dump, sent_data, user_table
 
 def subscribe_email(email):
-    pass
+    token = "abcdef" # TODO: use a md5 hash here
+    user_table.insert({
+        "email": email,
+        "token": token,
+        "subscribed": False
+    })
+    return token
 
-def confirm(email):pass
+def confirm(email, token):
+    user = user_table.find_one({"email": email}, {"token": 1})
+    if user.token == token:
+        user_table.update({
+            "email": email
+        }, {
+            "subscribed": True
+        })
 
 def unsubscribe(email):pass
 
